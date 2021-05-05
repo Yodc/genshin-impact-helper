65c99527dff91d6e7ec7bb01b7087088a94c0bab
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-05-05 14:10:55 +0700

    change delay to 3 sec

--- genshin-os.py
+++ genshin-os.py
@@ -91,7 +91,7 @@ class Sign(Base):
                 str(self.uid)[1:7], '******', 1)
 
             log.info(f'Checking in account id {uid}...')
-            time.sleep(10)
+            time.sleep(3)
             message = {
                 'today': today,
                 'region_name': region_name,
